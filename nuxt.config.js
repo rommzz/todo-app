c11a709430997b4b89bc71ab3197ebfe80142fbd
rommzz <rommy_zohara@yahoo.com>
2021-09-01 12:41:47 +0700

    init

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'todo-app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+
+  ],
+
+	firebase: {
+		apiKey: "AIzaSyDkHp81OBJqRmsiJjNFh_uzOH7OgDL51BQ",
+		authDomain: "todo-app-4dde8.firebaseapp.com",
+		projectId: "todo-app-4dde8",
+		storageBucket: "todo-app-4dde8.appspot.com",
+		messagingSenderId: "174859656424",
+		appId: "1:174859656424:web:606009103574c229753d5d",
+		measurementId: "G-K9CEJ6VEQ7"
+	},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
